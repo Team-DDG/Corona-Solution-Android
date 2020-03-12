14399bf47fc06a1c72fec68d1b309d23aedb8a35
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-03-12 11:42:12 +0900

    [Add] Refactor MainViewModel in MVVM Pattern, Make FabAnimation files. modify gradle files

--- app/src/main/java/com/golddog/mask_location/ui/activity/MainActivity.kt
+++ app/src/main/java/com/golddog/mask_location/ui/activity/MainActivity.kt
@@ -1,87 +1,48 @@
 package com.golddog.mask_location.ui.activity
 
-import android.content.Intent
 import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
 import android.text.Spannable
 import android.text.style.ForegroundColorSpan
-import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.toSpannable
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.golddog.mask_location.R
 import com.golddog.mask_location.data.local.SharedPreference
 import com.golddog.mask_location.databinding.ActivityMainBinding
-import com.golddog.mask_location.util.FabAnimation
 import com.golddog.mask_location.util.showToast
 import com.golddog.mask_location.viewmodel.MainViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.android.synthetic.main.activity_main.*
 import net.daum.mf.map.api.MapView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
-    private var isFabOpen = false
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
-    private val preference by lazy {
-        SharedPreference.getInstance(applicationContext)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
-        setupFab()
+        val agreement = Observer<Boolean> {
+            if (!it) {
+                setupAgreementDialog().show()
+            }
+        }
+
+        viewModel.agreement.observe(this, agreement)
         setupMap()
-
-        if (!preference?.getAgreement()!!) {
-            setupAgreementDialog().show()
-        }
-    }
-
-    override fun onClick(view: View?) {
-        when (view) {
-            fab_main_main -> {
-                fabAnim()
-                showToast("fab_1")
-            }
-            fab_mask_main -> {
-                fabAnim()
-                showToast("fab_2")
-            }
-            fab_1339call_main -> {
-                fabAnim()
-                startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:1339")))
-            }
-            fab_corona_manual_main -> {
-                fabAnim()
-                showToast("fab_4")
-            }
-            fab_corona_now_main -> {
-                fabAnim()
-                showToast("fab_5")
-            }
-        }
-    }
-
-    private fun setupFab() {
-        fab_main_main.setOnClickListener(this)
-        fab_mask_main.setOnClickListener(this)
-        fab_1339call_main.setOnClickListener(this)
-        fab_corona_manual_main.setOnClickListener(this)
-        fab_corona_now_main.setOnClickListener(this)
     }
 
     private fun setupMap() {
-//        val mapView = MapView(this)
         val mapViewContainer = findViewById<ViewGroup>(R.id.map_view)
         mapViewContainer.addView(MapView(this))
     }
@@ -102,44 +63,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             .setPositiveButton(
                 R.string.agree
             ) { _, _ ->
-                preference?.setAgreement(true)
+                binding.vm?.setLocalAgreementData(true)
                 showToast("서비스 사용 서약에 동의했습니다.")
             }
             .setCancelable(false)
         // 람다로 작성함, 기능에 대해 변경해야 될 사항이 있다면, 중괄호 안에 확장해서 사용
     }
 
-    private fun fabAnim() {
-        if (isFabOpen) {
-            val fabClose = FabAnimation.fabClose()
-            val fabRotateBackward = FabAnimation.fabRotateBackward()
-
-            fab_main_main.startAnimation(fabRotateBackward)
-            fab_mask_main.startAnimation(fabClose)
-            fab_1339call_main.startAnimation(fabClose)
-            fab_corona_manual_main.startAnimation(fabClose)
-            fab_corona_now_main.startAnimation(fabClose)
-
-            fab_mask_main.isClickable = false
-            fab_1339call_main.isClickable = false
-            fab_corona_manual_main.isClickable = false
-            fab_corona_now_main.isClickable = false
-            isFabOpen = false
-        } else {
-            val fabRotateForward = FabAnimation.fabRotateFoward()
-            val fabOpen = FabAnimation.fabOpen()
-
-            fab_main_main.startAnimation(fabRotateForward)
-            fab_mask_main.startAnimation(fabOpen)
-            fab_1339call_main.startAnimation(fabOpen)
-            fab_corona_manual_main.startAnimation(fabOpen)
-            fab_corona_now_main.startAnimation(fabOpen)
-
-            fab_mask_main.isClickable = true
-            fab_1339call_main.isClickable = true
-            fab_corona_manual_main.isClickable = true
-            fab_corona_now_main.isClickable = true
-            isFabOpen = true
-        }
-    }
+//    private fun fabAnim() {
+//        if (isFabOpen) {
+//            val fabClose = FabAnimation.fabClose()
+//            val fabRotateBackward = FabAnimation.fabRotateBackward()
+//
+//            fab_main_main.startAnimation(fabRotateBackward)
+//            fab_mask_main.startAnimation(fabClose)
+//            fab_1339call_main.startAnimation(fabClose)
+//            fab_corona_manual_main.startAnimation(fabClose)
+//            fab_corona_now_main.startAnimation(fabClose)
+//
+//            fab_mask_main.isClickable = false
+//            fab_1339call_main.isClickable = false
+//            fab_corona_manual_main.isClickable = false
+//            fab_corona_now_main.isClickable = false
+//            isFabOpen = false
+//        } else {
+//            val fabRotateForward = FabAnimation.fabRotateForward()
+//            val fabOpen = FabAnimation.fabOpen()
+//
+//            fab_main_main.startAnimation(fabRotateForward)
+//            fab_mask_main.startAnimation(fabOpen)
+//            fab_1339call_main.startAnimation(fabOpen)
+//            fab_corona_manual_main.startAnimation(fabOpen)
+//            fab_corona_now_main.startAnimation(fabOpen)
+//
+//            fab_mask_main.isClickable = true
+//            fab_1339call_main.isClickable = true
+//            fab_corona_manual_main.isClickable = true
+//            fab_corona_now_main.isClickable = true
+//            isFabOpen = true
+//        }
+//    }
 }
--- app/src/main/java/com/golddog/mask_location/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/golddog/mask_location/viewmodel/MainViewModel.kt
@@ -1,7 +1,52 @@
 package com.golddog.mask_location.viewmodel
 
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.golddog.mask_location.base.BaseApplication
+import com.golddog.mask_location.data.local.SharedPreference
+import com.golddog.mask_location.util.showToast
 
 class MainViewModel : ViewModel() {
+    private val preference by lazy {
+        BaseApplication.appContext?.let { SharedPreference.getInstance(it) }
+    }
 
+    var fabOpen: MutableLiveData<Boolean> = MutableLiveData(false)
+    val agreement: MutableLiveData<Boolean> = MutableLiveData(preference!!.getAgreement())
+
+    fun setLocalAgreementData(agreement: Boolean) {
+        preference?.setAgreement(agreement)
+    }
+
+    fun clickFabMain() {
+        setFabOpen()
+        showToast("fab_1")
+    }
+
+    fun clickFabMask() {
+        setFabOpen()
+        showToast("fab_2")
+    }
+
+    fun clickFabCall() {
+        setFabOpen()
+        BaseApplication.appContext?.startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:1339")))
+    }
+
+    fun clickFabManualCorona() {
+        setFabOpen()
+        showToast("fab_4")
+
+    }
+
+    fun clickFabCurrentCorona() {
+        setFabOpen()
+        showToast("fab_5")
+    }
+
+    private fun setFabOpen() {
+        this.fabOpen.value = !this.fabOpen.value!!
+    }
 }
