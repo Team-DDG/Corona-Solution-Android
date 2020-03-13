b32b357bf47ab38c5204eba8495dc9efb78033e8
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-03-13 21:55:44 +0900

    [Add] Apply Review

--- app/src/main/java/com/golddog/mask_location/data/api/DrugStoreApi.kt
+++ app/src/main/java/com/golddog/mask_location/data/api/DrugStoreApi.kt
@@ -1,4 +1,23 @@
 package com.golddog.mask_location.data.api
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 interface DrugStoreApi {
+    companion object {
+        private const val baseUrl = "https://api.dropper.tech/covid19/status/korea"
+
+        private lateinit var retrofit : Retrofit
+
+        fun createRetrofit(): Retrofit {
+            retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return retrofit
+        }
+    }
 }
