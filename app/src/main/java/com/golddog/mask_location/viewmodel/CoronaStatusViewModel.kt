b89f2cee3a7041915d6088143da8d87998c8ad44
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-03-15 02:06:30 +0900

    [Refactor] Make Api Key Interceptor

--- app/src/main/java/com/golddog/mask_location/viewmodel/CoronaStatusViewModel.kt
+++ app/src/main/java/com/golddog/mask_location/viewmodel/CoronaStatusViewModel.kt
@@ -6,7 +6,6 @@ import com.golddog.mask_location.data.datasource.StatusDataSource
 import com.golddog.mask_location.entity.CoronaData
 import com.golddog.mask_location.entity.CoronaList
 import io.reactivex.disposables.CompositeDisposable
-import java.text.DecimalFormat
 
 class CoronaStatusViewModel(private val statusDataSource: StatusDataSource) : ViewModel() {
     var coronaList: MutableLiveData<CoronaList> = MutableLiveData(CoronaList(listOf(CoronaData(0, 0, 0, 0))))
@@ -19,7 +18,7 @@ class CoronaStatusViewModel(private val statusDataSource: StatusDataSource) : Vi
 
 
     private fun setAccumulateData() {
-        val accumulateDataDisposable = statusDataSource.getAccumulateData()
+        val accumulateDataDisposable = statusDataSource.getCoronaStatusData()
             .subscribe({
                 coronaList.postValue(it)
                 val patientData = it.data[0].certified - it.data[0].cure - it.data[0].dead
