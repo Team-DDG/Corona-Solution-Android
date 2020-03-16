1bd5ff47c7db1300544fdee842bb47db5ef5ba60
seung min <3235tmdals@gmail.com>
2020-03-17 00:58:36 +0900

    [Error] binding adapter set text color is not called after number picker changed value

---
+++ app/src/main/java/com/golddog/mask_location/viewmodel/MaskViewModel.kt
@@ -0,0 +1,17 @@
+package com.golddog.mask_location.viewmodel
+
+import android.app.Application
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.golddog.mask_location.R
+
+class MaskViewModel(private val application: Application): ViewModel(){
+    var year = MutableLiveData<Int>()
+    var monday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
+    var tuesday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
+    var wednesday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
+    var thursday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
+    var friday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
+}
