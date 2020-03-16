60e58f2f9653a33e68db1eff5d20db4677014804
seung min <3235tmdals@gmail.com>
2020-03-17 01:53:42 +0900

    [Error] binding adapter function's when is not working

--- app/src/main/java/com/golddog/mask_location/viewmodel/MaskViewModel.kt
+++ app/src/main/java/com/golddog/mask_location/viewmodel/MaskViewModel.kt
@@ -9,9 +9,4 @@ import com.golddog.mask_location.R
 
 class MaskViewModel(private val application: Application): ViewModel(){
     var year = MutableLiveData<Int>(-1)
-    var monday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
-    var tuesday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
-    var wednesday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
-    var thursday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
-    var friday: MutableLiveData<Int> = MutableLiveData(ContextCompat.getColor(application, R.color.colorSecondaryDark))
 }
