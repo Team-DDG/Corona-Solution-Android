694197db0c561b2dec0c79ba4931fda15f0d0ccb
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-03-20 05:12:44 +0900

    [Refactor] Set City Server Data to RecyclerView (No Design)

--- app/src/main/java/com/golddog/mask_location/viewmodel/CoronaStatusViewModel.kt
+++ app/src/main/java/com/golddog/mask_location/viewmodel/CoronaStatusViewModel.kt
@@ -1,34 +1,46 @@
 package com.golddog.mask_location.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.golddog.mask_location.data.datasource.StatusDataSource
 import com.golddog.mask_location.entity.AccumulateCoronaData
+import com.golddog.mask_location.entity.CityCoronaData
+import com.golddog.mask_location.entity.CityStatus
 import com.golddog.mask_location.entity.CoronaResult
 import io.reactivex.disposables.CompositeDisposable
 
 class CoronaStatusViewModel(private val statusDataSource: StatusDataSource) : ViewModel() {
-    var coronaList: MutableLiveData<AccumulateCoronaData> = MutableLiveData(
-        AccumulateCoronaData(
-            "갱신 중...",
-            CoronaResult()
-        )
-    )
+    val accumulateData: MutableLiveData<AccumulateCoronaData> = MutableLiveData(AccumulateCoronaData("갱신 중...", CoronaResult()))
+    val citiesData: MutableLiveData<CityCoronaData>
+            = MutableLiveData(CityCoronaData("갱신 중...", mutableListOf()))
     private val disposable = CompositeDisposable()
 
     init {
         setAccumulateData()
+        setCitiesData()
     }
 
     private fun setAccumulateData() {
         val accumulateDataDisposable = statusDataSource.getAccumulateData()
             .subscribe({
-                coronaList.value = it
+                accumulateData.value = it
             }) {
-                coronaList.value?.baseDate = "갱신 실패"
+                accumulateData.value?.baseDate = "갱신 실패"
             }
 
         disposable.add(accumulateDataDisposable)
+    }
+
+    private fun setCitiesData() {
+        val citiesDataDisposable = statusDataSource.getCitiesData()
+            .subscribe({
+                it.cityStatuses.add(0, CityStatus("지역명", "누적 확진환자", "사망자"))
+                citiesData.value = it
+            }) {
+                citiesData.value?.baseDate = "갱신 실패"
+            }
+        disposable.add(citiesDataDisposable)
     }
 
     override fun onCleared() {
