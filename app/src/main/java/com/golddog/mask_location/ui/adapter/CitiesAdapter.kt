718d231d26a5adf25dc67ebfbe309490edf1909b
seung min <3235tmdals@gmail.com>
2020-03-20 18:31:14 +0900

    [Modify] fixed recycler view ui and naver map

--- app/src/main/java/com/golddog/mask_location/ui/adapter/CitiesAdapter.kt
+++ app/src/main/java/com/golddog/mask_location/ui/adapter/CitiesAdapter.kt
@@ -1,13 +1,19 @@
 package com.golddog.mask_location.ui.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.golddog.mask_location.R
+import com.golddog.mask_location.base.BaseApplication
 import com.golddog.mask_location.databinding.ItemCitiesStatusBinding
 import com.golddog.mask_location.entity.CityStatus
 import com.golddog.mask_location.util.ItemBindDIffCallback
@@ -36,7 +42,7 @@ class CitiesAdapter: RecyclerView.Adapter<CitiesAdapter.CitiesViewHolder<ItemCit
         holder: CitiesViewHolder<ItemCitiesStatusBinding>,
         position: Int
     ) {
-        holder.binding().status = cityStatus[position]
+        holder.binding(position).status = cityStatus[position]
     }
 
     override fun getItemCount(): Int {
@@ -45,7 +51,27 @@ class CitiesAdapter: RecyclerView.Adapter<CitiesAdapter.CitiesViewHolder<ItemCit
 
     inner class CitiesViewHolder<T: ViewDataBinding>(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding: T = (DataBindingUtil.bind(itemView) as T?)!!
-        fun binding(): T {
+
+        private val container = itemView.findViewById<ConstraintLayout>(R.id.layout_list)
+        private val city_name = itemView.findViewById<TextView>(R.id.tv_city_name_list)
+        private val city_accomulate = itemView.findViewById<TextView>(R.id.tv_confirmed_list)
+        private val city_dead = itemView.findViewById<TextView>(R.id.tv_dead_list)
+        private val textColor = Color.BLACK
+
+        fun binding(position: Int): T {
+            val backgroundColor =
+                if ((position % 2) == 0) ContextCompat.getColor(BaseApplication.appContext!!, R.color.colorSecondaryLight)
+                else ContextCompat.getColor(BaseApplication.appContext!!, R.color.colorSecondaryDark)
+            container.background = backgroundColor.toDrawable()
+            city_name.setTextColor(textColor)
+            city_accomulate.setTextColor(textColor)
+            city_dead.setTextColor(textColor)
+            if (position == 0){
+                container.background = ContextCompat.getDrawable(BaseApplication.appContext!!, R.drawable.corona_status_city_top_background)
+                city_name.setTextColor(Color.WHITE)
+                city_accomulate.setTextColor(Color.WHITE)
+                city_dead.setTextColor(Color.WHITE)
+            }
             return binding
         }
     }
