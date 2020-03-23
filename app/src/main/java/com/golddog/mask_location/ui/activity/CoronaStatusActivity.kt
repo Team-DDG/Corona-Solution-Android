ada14f924ad677d53af607e0ce04542e760307c2
kimdohun0104 <dikolight203@gmail.com>
2020-03-23 15:34:21 +0900

    [Refactor] Move common code to BaseActivity.kt

--- app/src/main/java/com/golddog/mask_location/ui/activity/CoronaStatusActivity.kt
+++ app/src/main/java/com/golddog/mask_location/ui/activity/CoronaStatusActivity.kt
@@ -5,24 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.golddog.mask_location.R
+import com.golddog.mask_location.base.BaseActivity
 import com.golddog.mask_location.data.ApiClient
 import com.golddog.mask_location.databinding.ActivityCoronaStatusBinding
 import com.golddog.mask_location.viewmodel.CoronaStatusViewModel
 import com.golddog.mask_location.viewmodelfactory.CoronaStatusViewModelFactory
 
-class CoronaStatusActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCoronaStatusBinding
+class CoronaStatusActivity : BaseActivity<ActivityCoronaStatusBinding>() {
+    override val layoutResourceId: Int = R.layout.activity_corona_status
+
     private lateinit var viewModel: CoronaStatusViewModel
     private lateinit var viewModelFactory: CoronaStatusViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_corona_status)
         viewModelFactory = CoronaStatusViewModelFactory(ApiClient())
         viewModel = ViewModelProvider(this, viewModelFactory).get(CoronaStatusViewModel::class.java)
 
         binding.vm = viewModel
-        binding.lifecycleOwner = this
     }
 }
